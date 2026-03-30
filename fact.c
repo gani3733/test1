f4cb9c82f2ad55a81b8e829d7ad75ec7ddf27e33
EC2 Default User <ec2-user@ip-172-31-38-94.ap-south-1.compute.internal>
2026-03-30 17:18:48 +0000

    c project

---
+++ fact.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact()
+{
+    int i, n;
+    long fac = 1;
+    
+    printf("\n\nPlease Enter any number to Find Factorial = ");
+    scanf("%d", &n);
+    
+    for (i = 1; i <= n; i++)
+    {
+        fac = fac * i;
+    }
+    printf("\nFactorial of %d = %ld\n", n, fac);
+    
+//    return 0;
+}
